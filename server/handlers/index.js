f73713af368ac01dbcea7fb397084a69b04ecc13
hyunju@Julie-Kim <hyunju.kim520@gmail.com>
2019-06-07 16:24:53 +1000

    initial commit

---
+++ server/handlers/index.js
@@ -0,0 +1,12 @@
+module.exports.notFound = (req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+
+  next(error);
+};
+
+module.exports.errors = (error, req, res, next) => {
+  res.status(error.status || 500).json({
+    error: error.message || "Something went wrong"
+  });
+};
