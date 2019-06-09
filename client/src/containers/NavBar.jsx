704e5a3bdd6e35d918115c14409ffef73f2ae70a
hyunju@Julie-Kim <hyunju.kim520@gmail.com>
2019-06-09 12:19:40 +1000

    added logout into NavBar

--- client/src/containers/NavBar.jsx
+++ client/src/containers/NavBar.jsx
@@ -1,5 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { logout } from "../store/actions";
 
-const NavBar = () => <div>NavBar works</div>;
+const NavBar = ({ auth, logout }) => (
+  <div>
+    <ul>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+      <li>
+        <a onClick={logout}>Logout</a>
+      </li>
+    </ul>
+    {auth.isAuthenticated && <p>Logged in as {auth.user.username}</p>}
+  </div>
+);
 
-export default NavBar;
+export default connect(
+  store => ({ auth: store.auth }),
+  { logout }
+)(NavBar);
